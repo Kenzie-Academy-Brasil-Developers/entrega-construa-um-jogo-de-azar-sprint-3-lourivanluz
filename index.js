42856e1a9f6bf7e63e36c6081b9c922806647f9a
lourivanrluz <lourivan-rodrigues@hotmail.com>
2021-07-16 14:10:34 -0300

    Adicionado Animacoes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const selectPlay = document.querySelectorAll('.select_player')
 const jogar = document.querySelector('#jogar')
-const jogadas = ['pedra','papel','tesoura']
+const opcoesJogadaArray = ['pedra','papel','tesoura']
 
 const selectPlayerArray = [...selectPlay]
 let imagem = ''
@@ -12,53 +12,95 @@ let rodada = 0
 for(let i = 0; i<selectPlayerArray.length;i++){
     let elemento = selectPlayerArray[i]
     elemento.addEventListener('click',(e)=>{
+        const circulosPequenos = document.querySelectorAll('.ciculoApresentacao--pequeno')
+        const circulosPequenosArray = [... circulosPequenos]
 
+        circulosPequenosArray.map((item)=>{
+            if (item.classList.contains('ativado')){
+                item.classList.remove('ativado')
+            }
+        })
         const playEu = document.querySelector('#playEU')
         const playIa = document.querySelector('#playIA')
-        playIa.setAttribute('src', "./src/pedra.png")
-
-        imagem = e.target.getAttribute('src')
-        alt = e.target.getAttribute('alt')
-
-        playEu.setAttribute('src', imagem)
+        alt = e.target.getAttribute('alt') 
         playEu.setAttribute('alt', alt)
-        console.log(playEu)
-        console.log(playIa)
-
-        
+        let divpai = e.target.parentElement.parentElement
+        divpai.classList.add('ativado') 
     })
 }
 
-jogar.addEventListener('click',()=>{
 
+jogar.addEventListener('click',()=>{
+ 
     const playEu = document.querySelector('#playEU')
     const playIa = document.querySelector('#playIA')
-    let index = getARondomNumber(0,2)
-    let selectIA = jogadas[index]
-    let selectEU = playEu.getAttribute('alt')
-    playIa.setAttribute('src', "./src/"+selectIA+".png")
-    console.log(playIa)
+    const divCirculoApresentacaoEu = playEu.parentElement.parentElement
+    const divCirculoApresentacaoIA = playIa.parentElement.parentElement
+    const divPlacarEu = document.querySelector('.placar__eu')
+    const divPlacarIA = document.querySelector('.placar__ia')
     
+    let index = getARondomNumber(0,2)
+    let selectIA = opcoesJogadaArray[index]
+    let selectEU = playEu.getAttribute('alt')
+
+    let imagemIA = "./src/"+selectIA+".png"
+    let imagemEU = "./src/"+selectEU+".png"
+
+    alterarImagemComDalay(playIa,'./src/pedra.png',0)
+    alterarImagemComDalay(playEu,'./src/pedra.png',0)
+    
+    animaElemento(divCirculoApresentacaoEu,'ciculoAnimacaoGrande',900)
+    animaElemento(divCirculoApresentacaoIA,'ciculoAnimacaoGrande',900)
+
+    alterarImagemComDalay(playIa,imagemIA,900)
+    alterarImagemComDalay(playEu,imagemEU,900)
+  
+
     if(selectIA ==='pedra'&& selectEU==='tesoura'||selectIA ==='papel'&& selectEU==='pedra'||selectIA ==='tesoura'&& selectEU==='papel'){
         pontuacaoIA++
+        animaElemento(divPlacarIA,'placar--animado',1500)
+        //ativar a div pra pintar de verde
 
-    }else if (selectEU===selectIA) {
-
-    }else{
+    }else if (selectEU!==selectIA) {
         pontuacaoEu++
-
+        animaElemento(divPlacarEu,'placar--animado',1500)
+        //ativar a div pra pintar de verde
     }
     rodada++
-    atualizaDados(pontuacaoEu,pontuacaoIA,rodada)
+    
+    atualizaDados(pontuacaoEu,pontuacaoIA,rodada,900)
 })
+
 
 
 function getARondomNumber(min,max){
     return Math.floor(Math.random()*(max-min+1) + min)
 }
 
-function atualizaDados(pontuacaoEu,pontuacaoIA,rodada){
-    document.querySelector('#placar__eu').innerHTML = pontuacaoEu
-    document.querySelector('#placar__ia').innerHTML = pontuacaoIA
-    document.querySelector('#rodada').innerHTML = rodada
+function atualizaDados(pontuacaoEu,pontuacaoIA,rodada,dalay){
+    setTimeout(()=>{
+        document.querySelector('#placar__eu').innerHTML = pontuacaoEu
+        document.querySelector('#placar__ia').innerHTML = pontuacaoIA
+        document.querySelector('#rodada').innerHTML = rodada
+
+    },dalay)
+    
 }
+
+
+function animaElemento(elemento,classAnimada,dalay){
+    elemento.classList.add(classAnimada)
+    setTimeout(()=>{
+        elemento.classList.remove(classAnimada)
+
+    },dalay)  
+}
+
+function alterarImagemComDalay(elemento,imagem,dalay){
+    setTimeout(()=>{
+        elemento.setAttribute('src', imagem)
+    },dalay)
+}
+
+
+  
